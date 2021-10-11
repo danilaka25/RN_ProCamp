c5377b796b9b59393a0ff9531509db8eaea00eca
danilaka25 <danilaka25@gmail.com>
2021-10-11 17:07:18 +0300

    Profile screen

---
+++ src/components/contactsField/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { View , Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+ 
+
+interface Props {
+  icon: String;
+  text: String;
+}
+
+const ContactsField = ({...props }: Props) => (
+  <View style={styles.fieldWrapper}>
+      {props.icon ?  <Ionicons name={props.icon} size={22} color="#000" style={styles.fieldIcon} /> : null}
+      <Text>{props.text}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  fieldWrapper: {
+    backgroundColor: '#f3efef',
+    width: 200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 3,
+    paddingHorizontal: 9,
+    borderRadius: 5,
+    marginBottom: 15
+  },
+  fieldIcon: {
+    marginRight: 10
+  }
+
+  
+});
+
+export default ContactsField;
